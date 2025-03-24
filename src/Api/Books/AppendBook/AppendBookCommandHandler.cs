41092a21dd3ada950f6690d9a038376eae97b2d9
iksergey <i@ksergey.ru>
2025-03-24 19:34:15 +0300

    012 AppendBookEndpoint и вся инфраструктура

---
+++ src/Api/Books/AppendBook/AppendBookCommandHandler.cs
@@ -0,0 +1,36 @@
+namespace Api.Books.AppendBook;
+
+public record AppendBookCommand(
+    string Title,
+    string Name,
+    string Description,
+    string ImageUrl,
+    decimal Price,
+    List<string> Category
+) : ICommand<AppendBookResult>;
+
+public record AppendBookResult(Guid Id);
+
+public class AppendBookCommandHandler(IDocumentSession session)
+    : ICommandHandler<AppendBookCommand, AppendBookResult>
+{
+    public async Task<AppendBookResult> Handle(
+        AppendBookCommand command,
+        CancellationToken cancellationToken)
+    {
+        var book = new Book
+        {
+            Title = command.Title,
+            Name = command.Name,
+            Description = command.Description,
+            ImageUrl = command.ImageUrl,
+            Price = command.Price,
+            Category = command.Category
+        };
+
+        session.Store(book);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new AppendBookResult(book.Id);
+    }
+}
