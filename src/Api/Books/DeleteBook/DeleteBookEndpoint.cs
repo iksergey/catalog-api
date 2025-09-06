555b0b8bf4c84aa2ec9a22bed3392a2273f9c0bf
iksergey <i@ksergey.ru>
2025-09-06 07:36:04 +0300

    019.2 Обновление маршрута для DeleteBook /books/{id}

--- src/Api/Books/DeleteBook/DeleteBookEndpoint.cs
+++ src/Api/Books/DeleteBook/DeleteBookEndpoint.cs
@@ -1,19 +1,17 @@
 namespace Catalog.API.Books.DeleteBook;
 
-public record DeleteBookRequest(string Id);
-
 public record DeleteBookResponse(bool IsSuccess);
 
 public class DeleteBookEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        // http://localhost:15000/books?id=10000001-0000-0000-0000-000000000001
-        app.MapDelete("/books", async (
-            [AsParameters] DeleteBookRequest request,
+        // http://localhost:15000/books/10000001-0000-0000-0000-000000000001
+        app.MapDelete("/books/{id}", async (
+            string id,
             ISender sender) =>
         {
-            DeleteBookCommand command = request.Adapt<DeleteBookCommand>();
+            DeleteBookCommand command = new DeleteBookCommand(id);
             DeleteBookResult result = await sender.Send(command);
             DeleteBookResponse response = result.Adapt<DeleteBookResponse>();
             return Results.Ok(response);
